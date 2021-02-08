aa62d577299dceac061722917173eba71c9c2e15
Ifthekar Alam <52175323+ifthekar9121@users.noreply.github.com>
2021-02-08 21:47:56 +0600

    Add files via upload

---
+++ src/includes/secondfooter.php
@@ -0,0 +1,35 @@
+<footer class="page-footer">
+         <div class="container footer-info">
+           <div class="row">
+             <div class="col s12 l4 ">
+               <ul>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="#top">To the Top</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="index">Home</a></li>
+               </ul>
+             </div>
+             <div class="col s12 l4 ">
+               <ul>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="sign">Sign In</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="404">Advertise on Lë JâMâ</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartPhone</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartCam</a></li>
+               </ul>
+             </div>
+
+             <div class="col s12 l4 ">
+               <ul>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartLaptop</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartWatch</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartSpeaker</a></li>
+                 <li><a class="grey-text text-lighten-3 animated slideInUp wow" href="category">SmartVR</a></li>
+               </ul>
+             </div>
+           </div>
+         </div>
+         <div class="footer-copyright">
+           <div class="container">
+           © <?= Date('Y'); ?> <strong><a class="white-text animated slideInUp wow" href="#">|| Lë JâMâ ||</a> </strong> All rights reserved
+           <!-- <a class="grey-text text-lighten-4 right animated slideInUp wow" href="http://smakosh.com">Made with <i class="fa fa-heartbeat animated pulse infinite red-text"></i> By Ismail Ghallou</a> -->
+           </div>
+         </div>
+       </footer>
